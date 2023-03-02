71a371132dbf50323be8182b99b9e3a551a43e2f
AlexBuena <126193871+AlexBuena@users.noreply.github.com>
2023-03-02 17:36:05 +0100

    Add files via upload

---
+++ teoria.sql
@@ -0,0 +1,24 @@
+Llenguatge SQL:  DCL(RA1) + DDL + DML
+
+DDL: (Llenguatge de definició de dades): CREATE TABLE, DROP TABLE, ALTER TABLE,
+TRUNCATE TABLE
+
+DML: (Llenguatge de manipulació de dades): INSERT, UPDATE, DELETE, SELECT, COMMIT
+, ROLLBACK;
+
+Llenguatge DCL: CREATE USER, GRANT, REVOKE, CREATE ROLE, CREATE PROFILE, DROP ROLE, DROP PROFILE
+
+RA2 (Extendre el SQL a llenguatge de programació, SQL-HOST, PL-SQL)
+
+Procedural Language (Standard query language)
+
+Instrucción 1;
+Instrucción 2;
+Instrucción 3;
+
+--- if (condicion) {si es verdad;}
+    else  {si es falso;}
+    
+--- while (condicion)
+(repetimos mientras condicion sea true);
+
